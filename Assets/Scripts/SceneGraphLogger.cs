38b0a2b29a217a9731a48ef76b2854c5007c1d09
Patrick Steinert <me@patrickbreucking.de>
2024-01-08 21:55:15 +0100

    Modified Scene Graph logging

--- Assets/Scripts/SceneGraphLogger.cs
+++ Assets/Scripts/SceneGraphLogger.cs
@@ -12,11 +12,18 @@ public class SceneGraphLogger : MonoBehaviour
     {
         Scene currentScene = SceneManager.GetActiveScene();
         GameObject[] rootObjects = currentScene.GetRootGameObjects();
-
+        Debug.Log("--- Start Scene Graph ---");
         foreach (GameObject obj in rootObjects)
         {
             LogGameObject(obj, 0);
         }
+        Debug.Log("--- End Scene Graph ---");
+        /*string output = "";
+        foreach (GameObject obj in rootObjects)
+        {
+                output = output + GameObjectsAsString(obj, 0);
+        }
+        Debug.Log(output);*/
     }
 
     private void Awake()
@@ -37,31 +44,54 @@ public class SceneGraphLogger : MonoBehaviour
         gameObject.SetActive(!isActive);
         Scene currentScene = SceneManager.GetActiveScene();
         GameObject[] rootObjects = currentScene.GetRootGameObjects();
-
+        string output = "";
         foreach (GameObject obj in rootObjects)
         {
-                LogGameObject(obj, 0);
+                output = output + GameObjectsAsString(obj, 0);
         }
+        Debug.Log(output);
+
     }
 
     void LogGameObject(GameObject obj, int depth)
     {
         string indent = new string(' ', depth * 2);
         Renderer renderer = obj.GetComponent<Renderer>();
+        string output = "";
         if (renderer != null && renderer.isVisible)
         {
-            Debug.Log($"1 {indent}{obj.name}");
+            output = $"1 {indent}{obj.name}";
         } else if (renderer != null) {
-            Debug.Log($"2 {indent}{obj.name}");
+            output = $"2 {indent}{obj.name}";
         } else if (renderer == null) {
-            Debug.Log("renderer null");
+            output = $"3 {indent}{obj.name}";
         }
-
-        //Debug.Log($"2 {indent}{obj.name}");
+        Debug.Log(output);
 
         foreach (Transform child in obj.transform)
         {
             LogGameObject(child.gameObject, depth + 1);
         }
+    }
+    string GameObjectsAsString(GameObject obj, int depth)
+    {
+        string indent = new string(' ', depth * 2);
+        Renderer renderer = obj.GetComponent<Renderer>();
+        string output = "";
+        if (renderer != null && renderer.isVisible)
+        {
+            output = $"1 {indent}{obj.name} \n";
+        } else if (renderer != null) {
+            output = $"2 {indent}{obj.name} \n";
+        } else if (renderer == null) {
+            output = $"3 {indent}{obj.name} \n";
+        }
+
+        foreach (Transform child in obj.transform)
+        {
+            output = output + GameObjectsAsString(obj, 0);
+        }
+        return output;
+        //Debug.Log(output);
     }
 }
